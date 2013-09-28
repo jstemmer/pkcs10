9f761bb16ba64456e750260d3ebcfd758433276c
Joël Stemmer <stemmertech@gmail.com>
2013-09-28 19:19:04 +0200

    Parse certificate signing requests

--- pkcs10.go
+++ pkcs10.go
@@ -1,1 +1,79 @@
 package pkcs10
+
+import (
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/asn1"
+)
+
+type certificateSigningRequest struct {
+	Raw                      asn1.RawContent
+	CertificationRequestInfo certificationRequestInfo
+	SignatureAlgorithm       pkix.AlgorithmIdentifier
+	SignatureValue           asn1.BitString
+}
+
+type certificationRequestInfo struct {
+	Raw           asn1.RawContent
+	Version       int
+	Subject       asn1.RawValue
+	SubjectPKInfo publicKeyInfo
+	Attributes    asn1.RawValue
+}
+
+type CertificateSigningRequest struct {
+	Raw                         []byte
+	RawCertificationRequestInfo []byte
+	RawSubject                  []byte
+	RawSubjectPublicKeyInfo     []byte
+
+	Signature          []byte
+	SignatureAlgorithm x509.SignatureAlgorithm
+
+	PublicKeyAlgorithm x509.PublicKeyAlgorithm
+	PublicKey          interface{}
+
+	Version int
+	Subject pkix.Name
+}
+
+func ParseCertificateSigningRequest(asn1Data []byte) (*CertificateSigningRequest, error) {
+	var csr certificateSigningRequest
+	rest, err := asn1.Unmarshal(asn1Data, &csr)
+	if err != nil {
+		return nil, err
+	}
+	if len(rest) > 0 {
+		return nil, asn1.SyntaxError{Msg: "trailing data"}
+	}
+
+	return parseCertificateSigningRequest(&csr)
+}
+
+func parseCertificateSigningRequest(in *certificateSigningRequest) (*CertificateSigningRequest, error) {
+	out := new(CertificateSigningRequest)
+	out.Raw = in.Raw
+	out.RawCertificationRequestInfo = in.CertificationRequestInfo.Raw
+	out.RawSubject = in.CertificationRequestInfo.Subject.FullBytes
+	out.RawSubjectPublicKeyInfo = in.CertificationRequestInfo.SubjectPKInfo.Raw
+
+	out.Signature = in.SignatureValue.RightAlign()
+	out.SignatureAlgorithm = getSignatureAlgorithmFromOID(in.SignatureAlgorithm.Algorithm)
+
+	out.PublicKeyAlgorithm = getPublicKeyAlgorithmFromOID(in.CertificationRequestInfo.SubjectPKInfo.Algorithm.Algorithm)
+	var err error
+	out.PublicKey, err = parsePublicKey(out.PublicKeyAlgorithm, &in.CertificationRequestInfo.SubjectPKInfo)
+	if err != nil {
+		return nil, err
+	}
+
+	out.Version = in.CertificationRequestInfo.Version
+
+	var subject pkix.RDNSequence
+	if _, err := asn1.Unmarshal(in.CertificationRequestInfo.Subject.FullBytes, &subject); err != nil {
+		return nil, err
+	}
+	out.Subject.FillFromRDNSequence(&subject)
+
+	return out, nil
+}
--- pkcs10_test.go
+++ pkcs10_test.go
@@ -1,1 +1,68 @@
 package pkcs10
+
+import (
+	"crypto/x509/pkix"
+	"encoding/pem"
+	"reflect"
+	"testing"
+)
+
+func TestParseCertificateSigningRequest(t *testing.T) {
+	block, _ := pem.Decode([]byte(csrNoAttrs))
+	csr, err := ParseCertificateSigningRequest(block.Bytes)
+	if err != nil {
+		t.Fatalf("Error parsing csr: %s", err)
+	}
+
+	if csr.Version != 0 {
+		t.Errorf("Invalid CSR version. Got %d, want %d", csr.Version, 0)
+	}
+
+	expectedSubject := pkix.Name{
+		Country:            []string{"NL"},
+		Organization:       []string{"Σ Acme Co"},
+		OrganizationalUnit: []string{"Unit"},
+		Locality:           []string{"City"},
+		Province:           []string{"Province"},
+		StreetAddress:      nil,
+		PostalCode:         nil,
+		SerialNumber:       "",
+		CommonName:         "test.example.com",
+		Names:              []pkix.AttributeTypeAndValue{},
+	}
+
+	if !reflect.DeepEqual(csr.Subject.Country, expectedSubject.Country) {
+		t.Errorf("Incorrect country. Got %v, want %v", csr.Subject.Country, expectedSubject.Country)
+	}
+
+	if !reflect.DeepEqual(csr.Subject.Organization, expectedSubject.Organization) {
+		t.Errorf("Incorrect organization. Got %v, want %v", csr.Subject.Organization, expectedSubject.Organization)
+	}
+
+	if !reflect.DeepEqual(csr.Subject.OrganizationalUnit, expectedSubject.OrganizationalUnit) {
+		t.Errorf("Incorrect organizational unit. Got %v, want %v", csr.Subject.OrganizationalUnit, expectedSubject.OrganizationalUnit)
+	}
+
+	if !reflect.DeepEqual(csr.Subject.Locality, expectedSubject.Locality) {
+		t.Errorf("Incorrect locality. Got %v, want %v", csr.Subject.Locality, expectedSubject.Locality)
+	}
+
+	if !reflect.DeepEqual(csr.Subject.Province, expectedSubject.Province) {
+		t.Errorf("Incorrect province. Got %v, want %v", csr.Subject.Province, expectedSubject.Province)
+	}
+
+	if csr.Subject.CommonName != expectedSubject.CommonName {
+		t.Errorf("Incorrect common name. Got %v, want %v", csr.Subject.CommonName, expectedSubject.CommonName)
+	}
+}
+
+var csrNoAttrs = `-----BEGIN CERTIFICATE REQUEST-----
+MIIBKDCB0wIBADBuMQswCQYDVQQGEwJOTDERMA8GA1UECAwIUHJvdmluY2UxDTAL
+BgNVBAcMBENpdHkxEzARBgNVBAoMCs6jIEFjbWUgQ28xDTALBgNVBAsMBFVuaXQx
+GTAXBgNVBAMMEHRlc3QuZXhhbXBsZS5jb20wXDANBgkqhkiG9w0BAQEFAANLADBI
+AkEAspkPScR9+ozUAK5qTRuKO2oTZCsj8osAO/uXeQremkzIK4sqgXR93sCLYpbl
+OgjDMWh+8lxL9JNrocDmBB6dFQIDAQABoAAwDQYJKoZIhvcNAQEFBQADQQAZOqEg
+pO+V1WGCGkBkGgmM2QlnrKaFYaRgYVlSEg7Tf+n9Wb8grcbQA8xo49z8qh2PbzgX
+M7Ib4RDKnANBH0R+
+-----END CERTIFICATE REQUEST-----
+`
